407fab11d3d4abbd49ee4b030a2771cf9f86dcd2
Jaaki <jaakidup@gmail.com>
2019-03-02 20:39:53 +0200

    API Services

---
+++ services/products.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 1,
+      price: 4,
+      title: 'Drinks',
+      imgUrl: 'http://res.cloudinary.com/christekh/image/upload/v1515183358/pro3_tqlsyl.png'
+    },
+    {
+      id: 2,
+      price: 3,
+      title: 'Home',
+      imgUrl: 'http://res.cloudinary.com/christekh/image/upload/v1515183358/pro2_gpa4su.png'
+    },
+    {
+      id: 3,
+      price: 1,
+      title: 'Stationary',
+      imgUrl: 'http://res.cloudinary.com/christekh/image/upload/v1515183358/pro1_m3d9mj.png'
+    },
+    {
+      id: 4,
+      price: 5,
+      title: 'Coffee',
+      imgUrl: 'http://res.cloudinary.com/christekh/image/upload/v1515183358/pro4_elakwz.png'
+    },
+    {
+      id: 5,
+      price: 7,
+      title: 'Notebooks',
+      imgUrl: 'http://res.cloudinary.com/christekh/image/upload/v1515183358/pro5_yvsj0v.png'
+    },
+    {
+      id: 6,
+      price: 6,
+      title: 'Business cards',
+      imgUrl: 'http://res.cloudinary.com/christekh/image/upload/v1515183359/pro6_qivlge.png'
+    }
+  ]
